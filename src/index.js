6c2b65b5be4f4635972badf7ef08cd7c1947452b
viktoriya-dot <201990kg@gmail.com>
2022-08-02 14:51:39 +0600

    feat: task finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+const OPEN_BRACKETS = ['(', '{', '[', '1', '3', '5']
+  const BRACKETS_PAIR = {
+    [')'] : '(',
+    ['}'] : '{',
+    [']'] : '[',
+    ['2'] : '1',
+    ['4'] : '3',
+    ['6'] : '5',
+   };
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+   if (OPEN_BRACKETS.includes(currentSymbol)) {
+    stack.push(currentSymbol)
+  } else if ('|78'.includes(currentSymbol)) {
+    if (stack.length === 0) {
+      stack.push(currentSymbol)
+    } else {
+        if (currentSymbol === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      }
+      } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+    let topStackElement = stack[stack.length - 1]
+    if (BRACKETS_PAIR[currentSymbol] === topStackElement) {
+      stack.pop()
+    } else {
+      return false
+
+    }
+  }
 }
+return stack.length === 0
+}
+
